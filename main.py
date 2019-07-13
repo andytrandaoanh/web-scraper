a5208ff626359c3bc90ab7e98d932bbca8cbedc1
andytrandaoanh <andytrandaoanh@gmail.com>
2019-07-13 12:13:09 +0700

    change extract word mechanism

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from system_handler import writeJSON, openDir
 
 
 
-def extractWord(mySpan):
-	regPat = r'>[\w\s.(),]+<'
-	pattern = re.compile(regPat)
-	finds = re.findall(pattern, str(mySpan))
-	if (finds):
-		strOut = str(finds[0])
-		strOut = strOut.replace('>', '')
-		strOut = strOut.replace('<', '')
-		return(strOut)
+def extractWord(mySpan, myPat):
+	inputString = str(mySpan)
+	pattern = re.compile(myPat)
+	matchObj = re.match(pattern, inputString)
+	if(matchObj):
+		temp = inputString.split(">")
+		myCont = temp[1].replace('\n', '')
+		myCont = myCont.replace('</span', '')
+		return myCont
+	
 
 
 def extractIPA(mySpan):
@@ -29,6 +30,17 @@ def checkNode(mySpan, myPat):
 	else:
 		return False
 
+def getOrigin(divs):
+	origin = ''
+	for div in divs:
+		if (checkNode(div, r'<div class="senseInnerWrapper">')):
+			ps = str(div).split("<p>")
+			if(ps[1]):
+				tempOrigin = ps[1].split("</p>")
+				if(tempOrigin[0]):
+					origin = tempOrigin[0]					
+					
+	return (str(origin))
 
 def getLexico(word):
 
@@ -39,10 +51,16 @@ def getLexico(word):
 
 	spans = soup.find_all("span")
 
+	#print(spans)
+	
+
+
+
 	wordObject = {
 		'word': None,
 		'meaning': [],
-		'phonetic': None
+		'phonetic': None,
+		'origin': None
 	}
 
 
@@ -57,30 +75,37 @@ def getLexico(word):
 			'inflection': []
 		}
 		if (checkNode(span, r'<span class="hw"')):
-			wordObject['word'] = extractWord(span)
+			wordObject['word'] = extractWord(span, '<span class="hw"')
 		if (checkNode(span, r'<span class="pos">')):			
-			meaningObj['category'] = extractWord(span)
+			meaningObj['category'] = extractWord(span, '<span class="pos">')
 			wordObject['meaning'].append(meaningObj)
 		if (checkNode(span, r'<span class="ind">')):
-			wordObject['meaning'][-1]['definition'].append(extractWord(span))
+			wordObject['meaning'][-1]['definition'].append(extractWord(span, '<span class="ind">'))
 		if (checkNode(span, r'<span class="inflection-text">')):
-			wordObject['meaning'][-1]['inflection'].append(extractWord(span).replace(",", "").strip())
+			wordObject['meaning'][-1]['inflection'].append(extractWord(span, '<span class="inflection-text">').replace(",", "").strip())
 		if (checkNode(span, r'<span class="syn">')):
-			syn = extractWord(span)
+			syn = extractWord(span, '<span class="syn">')
 			if(syn):
 				wordObject['meaning'][-1]['synonym'].append(syn.replace(',', '').strip())
 			
 		if (checkNode(span, r'<span class="phoneticspelling">')):
 			wordObject['phonetic'] = extractIPA(span)
+
+	divs = soup.find_all("div")
+	origin = getOrigin(divs)
+	wordObject['origin'] = origin
+
+
 	return(wordObject)
 
 
 if __name__ == "__main__":
 
-	WORD = "dance"
+	WORD = "aardvark"
 	dirOut = "E:/FULLTEXT/LEXICO" 
 	pathOut = dirOut + '/' +  WORD + ".json"
 	wordObject = getLexico(WORD)
+	#print(wordObject)
 	writeJSON(wordObject, pathOut)
 	openDir(dirOut)
 	
--- regx.py
+++ regx.py
@@ -1,9 +1,10 @@
 import re
 
 
-mySpan = """<span class="syn">, scoop up, spade, dig, excavate, move, shift, heap, spoon, ladle, toss</span>"""
+mySpan = """<span class="ind">A nocturnal badger-sized burrowing mammal of Africa, with long ears, a tubular snout, 
+and a long extensible tongue, feeding on ants and termites.</span>"""
 
-myPat = '<span class="phoneticspelling">'
+myPat = '<span class="ind">'
 
 
 
@@ -22,15 +23,21 @@ def extractIPA(mySpan):
 	print('/' + items[1] + '/')
 
 
-def extractWord(mySpan):
-	regPat = r'>[\w\s.(),]+<'
-	pattern = re.compile(regPat)
-	finds = re.findall(pattern, str(mySpan))
-	if (finds):
-		strOut = finds[0]
-		strOut = strOut.replace('>', '')
-		strOut = strOut.replace('<', '')
-		return(strOut)
+def extractWord(mySpan, myPat):
+	inputString = str(mySpan)
+	pattern = re.compile(myPat)
+	matchObj = re.match(pattern, inputString)
+	if(matchObj):
+		temp = mySpan.split(myPat)
+		myCont = temp[1].replace('\n', '')
+		myCont = myCont.replace('</span>', '')
+		return myCont
+	
+	#temp = str(mySpan).split(startSpan)
+	#pattern = re.compile(startSpan)
+	
+	#print(matchObj)
+	
 
 
-print(extractWord(mySpan))
+print(extractWord(mySpan, myPat))
