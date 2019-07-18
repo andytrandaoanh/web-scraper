77c502605508c3d936571668efc1068ae76f9229
andytrandaoanh <andytrandaoanh@gmail.com>
2019-07-18 08:21:27 +0700

    try detection algorythm

--- process_lexico.py
+++ process_lexico.py
@@ -15,8 +15,15 @@ def extractHW(sInput):
 
 def processLexico(contents):
 	soup = BeautifulSoup(contents, "lxml")
-	#hw = soup.find("span", {"class" : "hw"})
-			
+	
+	#step 1: retrieve all word heads
+	#each word head will form an entry
+	#hwList = soup.find_all("span", {"class" : "hw"})
+	
+	#first word head parent
+	#parents = hwList[0].parents
+	#for parent in parents:
+	#	print(parent, '\n')		
 	#if hasNumber(hw.text):
 	#	headWord, index = extractHW(hw.text)
 	#	print("head word:", headWord, "index", index)
@@ -32,11 +39,40 @@ def processLexico(contents):
 	#span =  hwList[0].find_next('span', {"class" : "pos"})
 	
 	#print(span, '\n')
-	sections = soup.find_all("section")
-	for sect in sections:
-		print(sect, '\n')
+	#sections = soup.find_all("section")
+	#for sect in sections:
+	#	print(sect, '\n')
 
 
+	#h2 <h2 class="hwg"><span class="hw" data-headword-id="a">
+	#entries = soup.find_all("h2", {"class" : "hwg"})	
+	#for entry in entries:
+	#	print(entry, '\n')
+	#firstH2 = entries[0]
+	#children = firstH2.children
+	#for child in children:
+	#	print(child, '\n')
+
+	# try to understand pos
+	# span class="pos">determiner</span>
+	# parent is <h3 class="ps pos"><span class="pos">determiner</span></h3> 
+
+	#pos_spans = soup.find_all("span", {"class" : "pos"})	
+	#for span in pos_spans:
+	#	print(span.parent, '\n')	
+
+	#h3 <h2 class="hwg"><span class="hw" data-headword-id="a">
+	#entries = soup.find_all("h3", {"class" : "ps pos"})	
+	#for entry in entries:
+	#	print(entry, '\n')
+	#firstH2 = entries[0]
+	#children = firstH2.children
+	#for child in children:
+	#	print(child, '\n')	
+
+	hwList = soup.find_all("span", {"class" : "hw"})
+	categories = hwList[0].find_next_all("h3", {"class" : "ps pos"})
+	print(categories)
 
 if __name__ == "__main__":
 	pathIn = "E:/FULLTEXT/LEXICO/a.html"
